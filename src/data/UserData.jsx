67128878d65d8663541c593075c073a26bf4edaf
sajid293r <sajid293r@gmail.com>
2025-02-14 23:03:21 +0500

    done

---
+++ src/data/UserData.jsx
@@ -0,0 +1,40 @@
+export const UserData = {
+  name: "Sajid nazir",
+  about : "I'm  software engineering that learned everything on my own. I specialize in creating dynamic, accessible websites that work on any device. Furthermore, I regularly create creative solutions that improve user experiences. I am proficient in React.js, Redux, JavaScript, HTML5, and CSS. I guarantee efficient code management and communication thanks to my knowledge with Git version control. Likewise, I'm also skilled at developing apps using mini program studio frameworks and exceeding client expectations with my high-caliber solutions.",
+  resumeUrl : "#",
+  FooterLink : "https://www.linkedin.com/company/craftabyte/",
+  socialMedia: [
+    {
+      socialMediaName: "github",
+      url: "https://github.com/sajid293r",
+      icon: "AiFillGithub",
+    },
+    {
+      socialMediaName: "linkedin",
+      url: "https://www.linkedin.com/company/craftabyte/",
+      icon: "FaLinkedinIn",
+    },
+    {
+      socialMediaName: "twitter",
+
+      icon: "AiOutlineTwitter",
+    },
+    {
+      socialMediaName: "facebook",
+      url: "https://www.facebook.com/share/p/1EdgWgpAbJ",
+      icon: "AiFillFacebook",
+    },
+  ],
+  typewriterOptions: {
+    strings: [
+      "Front End Developer",
+      "React Js Developer",
+      "React Native Developer",
+      "UI Developer",
+      "Javascript Developer",
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 30,
+  },
+};
